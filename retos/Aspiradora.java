4ab15bad4c4dc33634d1b1ebfb15b42c6d94f454
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2023-12-11 16:20:35 +0100

    Aspiradora con movimiento y añado mejora parcial

--- retos/Aspiradora.java
+++ retos/Aspiradora.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Aspiradora {
     public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+
     final String ZONA_LIMPIA = " . ";
     final String ZONA_SUCIA = "...";
     final String ZONA_MAS_SUCIA = "ooo";
@@ -9,12 +14,46 @@ public class Aspiradora {
     final String GATO = "\"^\"";
     final String SOFA = "[####]";
     final String MESA = "``";
+    final int COLUMNAS = 17;
+    final int FILAS = 14;
 
-        for (int filas = 1; filas <= 14; filas ++){
-            for (int columnas = 1; columnas <= 17; columnas++){
-                System.out.print(ZONA_LIMPIA);
+    int filaAspiradora = numeroAleatorio(1, FILAS);
+    int columnaAspiradora = numeroAleatorio(1, FILAS);
+
+    
+    while (1 == 1){
+        boolean aspiradoraDibujada = false;
+        for (int fila = 1; fila <= FILAS; fila ++){
+            for (int columna = 1; columna <= COLUMNAS; columna++){
+                if (fila == filaAspiradora && columna == columnaAspiradora && !aspiradoraDibujada){
+                    System.out.print(ASPIRADORA);
+                    aspiradoraDibujada = true;
+                    filaAspiradora = numeroAleatorio((filaAspiradora - 1), (filaAspiradora + 1));
+                    filaAspiradora = filaAspiradora < 1 ? 1 : filaAspiradora;
+                    filaAspiradora = filaAspiradora > FILAS ? FILAS : filaAspiradora;
+                    columnaAspiradora = numeroAleatorio((columnaAspiradora - 1), (columnaAspiradora + 1));
+                    columnaAspiradora = columnaAspiradora == 0 ? 1 : columnaAspiradora;
+                    columnaAspiradora = columnaAspiradora > FILAS ? FILAS : columnaAspiradora;
+                } else {
+                    System.out.print(ZONA_LIMPIA);
+                }
             }
             System.out.println();
         }
+        scanner.nextLine();
     }
+    }
+    
+    static int numeroAleatorio(int minimo, int maximo) {
+        return (int) (Math.random() * (maximo - minimo + 1) + minimo);
+    }
+
+    static boolean calculoProbabilidad(int probabilidad) {
+        if (numeroAleatorio(0, 100) < probabilidad){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
